0fc8c5cd218647f2cedb90d24fa4eff3c36eb03b
Aruwubato <albertwirz7777@gmail.com>
2024-01-10 22:34:34 +0100

    Endabgabe

---
+++ EndabgabeS1/script.js
@@ -0,0 +1,45 @@
+document.getElementById('button-container').onclick = function() {
+    alert('5 SECONDS');
+};
+
+document.getElementById("play1").addEventListener("click", function(){
+	var audio = document.getElementById('testAudio1');
+  if(this.className == 'is-playing'){
+    this.className = "";
+    this.innerHTML = "SONG 1"
+    audio.pause();
+  }else{
+    this.className = "is-playing";
+    this.innerHTML = "Pause";
+    audio.play();
+  }
+
+});
+
+document.getElementById("play2").addEventListener("click", function(){
+	var audio = document.getElementById('testAudio2');
+  if(this.className == 'is-playing'){
+    this.className = "";
+    this.innerHTML = "SONG 2"
+    audio.pause();
+  }else{
+    this.className = "is-playing";
+    this.innerHTML = "Pause";
+    audio.play();
+  }
+
+});
+
+document.getElementById("play3").addEventListener("click", function(){
+	var audio = document.getElementById('testAudio3');
+  if(this.className == 'is-playing'){
+    this.className = "";
+    this.innerHTML = "SONG 3"
+    audio.pause();
+  }else{
+    this.className = "is-playing";
+    this.innerHTML = "Pause";
+    audio.play();
+  }
+
+});
